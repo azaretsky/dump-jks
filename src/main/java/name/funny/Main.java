8a9e76319a2124fa632669595bd055ef1c742e5c
Anatoly Zaretsky <anatoly.zaretsky@gmail.com>
2020-02-11 01:31:39 +0200

    Fix typo in keystore file argument number

--- src/main/java/name/funny/Main.java
+++ src/main/java/name/funny/Main.java
@@ -9,7 +9,7 @@ public class Main {
     public static void main(String[] args) throws IOException, NoSuchAlgorithmException {
         char[] password = System.console().readPassword("Password: ");
         try (JKSDumper dumper = new JKSDumper(password);
-             InputStream inputStream = new FileInputStream(args[1])) {
+             InputStream inputStream = new FileInputStream(args[0])) {
             dumper.dumpJKS(inputStream);
         }
     }
